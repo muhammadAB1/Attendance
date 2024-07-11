d8591aed378c4581601fb528d03701383e0e2d00
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-07-11 21:11:26 +0500

    Add files via upload

---
+++ Madrassa App/Backend/dist/models/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import User from "./User.js";
+import { config } from "dotenv";
+import Dates from "./Dates.js";
+// mongoose.connect('mongodb+srv://muhammad:Digimon03.@cluster0.dek5f6q.mongodb.net/quiz?retryWrites=true&w=majority&appName=Cluster0')
+config();
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('connected to DB'))
+    .catch((e) => console.log(e));
+export const db = {
+    User,
+    Dates
+};
+//# sourceMappingURL=index.js.map
