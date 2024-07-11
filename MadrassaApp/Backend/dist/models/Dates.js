7d5a0b2ee93d9eb4c9396b057de1414ed63655d9
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-07-12 02:14:11 +0500

    Add files via upload

---
+++ MadrassaApp/Backend/dist/models/Dates.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const dateSchema = new mongoose.Schema({
+    dateToday: {
+        type: String,
+        unique: true
+    },
+    dayToday: {
+        type: String
+    }
+});
+export default mongoose.model('Date', dateSchema);
+//# sourceMappingURL=Dates.js.map
---
+++ MadrassaApp/Backend/dist/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const attendanceSchema = new mongoose.Schema({
+    attendance: {
+        type: String,
+    },
+    dateToday: {
+        type: String,
+        default: new Date().toLocaleDateString("en-US", { day: '2-digit', month: 'long', year: 'numeric', timeZone: 'Asia/Karachi' })
+    },
+});
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    darja: {
+        type: String,
+        required: true,
+    },
+    attendance: [attendanceSchema],
+    percentage: {
+        type: Number
+    }
+});
+export default mongoose.model('User', userSchema);
+//# sourceMappingURL=User.js.map
