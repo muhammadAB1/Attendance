6f7481f8c650ad6bb4c410fb34adc9356a9d2f45
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-07-12 02:13:00 +0500

    Delete Madrassa App directory

--- Madrassa App/Backend/dist/user-controller.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { db } from '../models/index.js';
-const userRoutes = express.Router();
-userRoutes.get('/', (req, res) => {
-    const data = db.User.find();
-    res.json(data);
-});
-export default userRoutes;
-//# sourceMappingURL=user-controller.js.map
