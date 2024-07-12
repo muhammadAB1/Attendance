a278392e8d52c21e45980f34010b9b9833f645c0
muhammadAB1 <151812244+muhammadAB1@users.noreply.github.com>
2024-07-12 16:29:33 +0500

    Update user-controller.js

--- MadrassaApp/API/src/controllers/user-controller.js
+++ MadrassaApp/API/src/controllers/user-controller.js
@@ -5,8 +5,13 @@ import { db } from '../models/index.js';
 const userRoutes = express.Router();
 
 userRoutes.get('/', async (req, res) => {
-    const data = await db.User.find();
-    res.json(data)
+    try {
+        const data = await db.User.find();
+        res.json(data)
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 userRoutes.post('/post', async (req, res) => {
@@ -22,22 +27,26 @@ userRoutes.post('/post', async (req, res) => {
     }
 })
 userRoutes.put('/postAttendance', async (req, res) => {
-    const { name, darja, attendance } = req.body;
-
-    const data = await db.User.findOne({ name: name, darja: darja })
-    if (!data) {
-        res.status(404).send('user not finded')
-    }
-    else {
-        const user = data.attendance.find((d) => d.dateToday === attendance.dateToday);
-        if (!user) {
-            data.attendance.push(attendance)
-            res.json(data)
-            await data.save();
+    try {
+        const { name, darja, attendance } = req.body;
+    
+        const data = await db.User.findOne({ name: name, darja: darja })
+        if (!data) {
+            res.status(404).send('user not finded')
         }
         else {
-            res.status(404).send('Attendace Already added')
+            const user = data.attendance.find((d) => d.dateToday === attendance.dateToday);
+            if (!user) {
+                data.attendance.push(attendance)
+                res.json(data)
+                await data.save();
+            }
+            else {
+                res.status(404).send('Attendace Already added')
+            }
         }
+    } catch (error) {
+        console.log(error)
     }
 })
 
@@ -77,31 +86,36 @@ userRoutes.put('/attendance', async (req, res) => {
 })
 
 userRoutes.post('/calculate', async (req, res) => {
-    const students = req.body;
-    const response = []
-
-    const calculatePercentage = (attendance) => {
-        const totalDays = attendance.length;
-        const presentDays = attendance.filter(att => att.attendance === 'present').length;
-        const percentage = (presentDays / totalDays) * 100;
-        return percentage.toFixed(2);
-    };
-    if (students.length === 0) {
-
-    }
-    for (let i = 0; i < students.length; i++) {
-        const user = await db.User.findOne({ name: students[i].name, darja: students[i].darja });
-        if (user) {
-            const percentage = calculatePercentage(user.attendance);
-            await db.User.updateOne({ name: students[i].name, darja: students[i].darja }, { percentage });
-            response.push({ name: students[i].name, status: 'percentage added' });
-
-        } else {
-            response.push({ name: students[i].name, status: 'User not found' });
+    try {
+        const students = req.body;
+        const response = []
+    
+        const calculatePercentage = (attendance) => {
+            const totalDays = attendance.length;
+            const presentDays = attendance.filter(att => att.attendance === 'present').length;
+            const percentage = (presentDays / totalDays) * 100;
+            return percentage.toFixed(2);
+        };
+        if (students.length === 0) {
+    
         }
-
+        for (let i = 0; i < students.length; i++) {
+            const user = await db.User.findOne({ name: students[i].name, darja: students[i].darja });
+            if (user) {
+                const percentage = calculatePercentage(user.attendance);
+                await db.User.updateOne({ name: students[i].name, darja: students[i].darja }, { percentage });
+                response.push({ name: students[i].name, status: 'percentage added' });
+    
+            } else {
+                response.push({ name: students[i].name, status: 'User not found' });
+            }
+    
+        }
+        res.json(response)
+        
+    } catch (error) {
+        console.log(error)        
     }
-    res.json(response)
 
 });
 
